20f3f5e9bb321530bb87e4ac3430bac1713846e0
Caleb Sandorf <cjsandorf@gmail.com>
2023-04-05 12:39:10 -0600

    Created peerProxy.js file, added necessary webSocket stuff to index.js.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { PeerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -114,9 +115,11 @@ function setAuthCookie(res, authToken) {
   });
 }
 
-app.listen(port, () => {
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+new PeerProxy(httpService);
 
 
 // let posts=[];
